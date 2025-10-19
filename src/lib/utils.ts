b7c3c1b34bcae3280ce3c536785bfe97f221b08c
RoeeRabinovich <roeerabinovitz@gmail.com>
2025-10-19 14:23:05 +0300

    Clean up: Remove old file structure

--- src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "flowbite-react/helpers/tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
